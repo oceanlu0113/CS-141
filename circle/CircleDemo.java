241802db4054ef423fc876a3bf4edccc1a538bfb
Ocean Lu <oceanlu0113@gmail.com>
2019-02-26 12:49:19 -0800

    Add files via upload

---
+++ circle/CircleDemo.java
@@ -0,0 +1,20 @@
+//Ocean Lu
+//CS 141
+//Assignment 1
+//10.11.17
+package circle;
+
+import java.util.Scanner;
+
+public class CircleDemo {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter a radius (doube): ");
+        double radius = keyboard.nextDouble();
+        Circle circ = new Circle(radius);
+        System.out.println("Area: " + circ.getArea());
+        System.out.println("Diameter: " + circ.getDiameter());
+        System.out.println("Circumference: " + circ.getCircumference());
+    }
+}
